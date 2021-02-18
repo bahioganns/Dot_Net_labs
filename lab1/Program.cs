517a46a9a5dd91c7f9ff7834d5dc41b92a2e6bac
Alex Kosh <sasha23969@gmail.com>
2021-02-18 14:45:55 +0300

    chore: Added more examples

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,6 +6,7 @@ namespace lab1
     {
         static void Main(string[] args)
         {
+            // Linked List Example
             LinkedList<int> list = new LinkedList<int>();
 
             list.push_back(123);
@@ -29,6 +30,26 @@ namespace lab1
 
             list.pop_back();
             Console.WriteLine(list);
+
+
+            // Binary Tree Example
+            BinaryTree.BinaryTree<int, int> tree = new BinaryTree.BinaryTree<int, int>();
+
+            tree.insert(2, 123);
+            tree.insert(1, 456);
+            tree.insert(3, 789);
+
+            tree.remove(2);
+
+            tree.find(2);
+            tree.find(1);
+            tree.find(3);
+
+
+            // Insertion Sort Example
+            int[] arr = { 2, 7, 3, 11, 6 };
+
+            MySort.InsertionSort(arr);
         }
     }
 }
