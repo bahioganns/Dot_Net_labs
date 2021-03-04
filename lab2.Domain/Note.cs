3f5021b0a8456abe438a401ce7b0ac4363e04d43
Alex Kosh <sasha23969@gmail.com>
2021-03-04 14:32:09 +0300

    refactor: Removing constructor from models

--- lab2.Domain/Note.cs
+++ lab2.Domain/Note.cs
@@ -4,15 +4,6 @@ namespace lab2.Domain
 {
     public class Note
     {
-        public Note(int id, int user_id, string title, string content, DateTime created)
-        {
-            this.id = id;
-            this.user_id = user_id;
-            this.title = title;
-            this.content = content;
-            this.created = created;
-        }
-
         public override string ToString()
         {
             return $"Note<id={id}, user_id='{user_id}', title='{title}'>";
--- lab2.Domain/User.cs
+++ lab2.Domain/User.cs
@@ -2,13 +2,6 @@ namespace lab2.Domain
 {
     public class User
     {
-        public User(int id, string email, string password_hash)
-        {
-            this.id = id;
-            this.email = email;
-            this.password_hash = password_hash;
-        }
-
         public override string ToString()
         {
             return $"User<id={id}, email='{email}'>";
