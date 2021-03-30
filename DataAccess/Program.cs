5688828a9f98582cb25e194f2dbc0c9750d5d8a6
Alex Kosh <sasha23969@gmail.com>
2021-03-30 21:00:09 +0300

    feat: Updated usage example

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -11,13 +11,17 @@ namespace DataLayer
     {
         static void Main(string[] args)
         {
-            var config = new MapperConfiguration(cfg => {
+            var mapperConfig = new MapperConfiguration(cfg => {
                 cfg.CreateMap<Domain.Models.UserUpdateModel, DataLayer.Entities.User>();
                 cfg.CreateMap<Domain.Models.UserUpdateModel, Domain.User>();
                 cfg.CreateMap<DataLayer.Entities.User, Domain.User>();
                 cfg.CreateMap<Domain.User, DataLayer.Entities.User>();
+                cfg.CreateMap<Domain.Models.NoteUpdateModel, DataLayer.Entities.Note>();
+                cfg.CreateMap<Domain.Models.NoteUpdateModel, Domain.Note>();
+                cfg.CreateMap<DataLayer.Entities.Note, Domain.Note>();
+                cfg.CreateMap<Domain.Note, DataLayer.Entities.Note>();
             });
-            Mapper mapper = new Mapper(config);
+            Mapper mapper = new Mapper(mapperConfig);
 
             var context = new NotesAppContext();
 
@@ -28,13 +32,37 @@ namespace DataLayer
             Console.WriteLine("Created User:");
             Console.WriteLine(user);
 
-            Console.WriteLine("\nGet from Database User:");
+            Console.WriteLine("\nGet User From Database:");
             Console.WriteLine(userDataAccess.Get(new UserIdentityModel(1)));
 
-            Domain.User updatedUser = userDataAccess.Update(new UserIdentityModel(1), new UserUpdateModel{ Email="111@789.ru", PasswordHash="rty" });
+            Domain.User updatedUser = userDataAccess.Update(
+                new UserIdentityModel(1),
+                new UserUpdateModel{ Email="111@789.ru", PasswordHash="rty" }
+            );
 
             Console.WriteLine("\nUpdated User:");
-            Console.Write(updatedUser);
+            Console.WriteLine(updatedUser);
+
+            NoteDataAccess noteDataAccess = new NoteDataAccess(context, mapper);
+
+            Domain.Note note = noteDataAccess.Insert(new NoteUpdateModel{ Title="123", Content="qwe", UserId=user.Id });
+
+            Console.WriteLine("\nCreated Note:");
+            Console.WriteLine(note);
+
+            Console.WriteLine("\nGet Note From Database:");
+            Console.WriteLine(noteDataAccess.Get(new NoteIdentityModel(1)));
+
+            Domain.Note updatedNote = noteDataAccess.Update(
+                new NoteIdentityModel(1),
+                new NoteUpdateModel{ Title="Test", Content="Hello", UserId=1 }
+            );
+
+            Console.WriteLine("\nUpdated Note:");
+            Console.WriteLine(noteDataAccess.Get(new NoteIdentityModel(1)));
+
+            Console.WriteLine("\nGet User Notes:");
+            noteDataAccess.GetUserNotes(new UserIdentityModel(1)).ForEach(Console.WriteLine);
         }
     }
 }
