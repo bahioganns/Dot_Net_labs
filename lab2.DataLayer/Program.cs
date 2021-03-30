f7196b3096be84d689065d889014043fd71ad9f7
Alex Kosh <sasha23969@gmail.com>
2021-03-30 19:07:42 +0300

    feat: Added usage example

--- lab2.DataLayer/Program.cs
+++ lab2.DataLayer/Program.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using lab2.DataLayer.Entities;
 using lab2.DataLayer.Context;
 using lab2.DataLayer.Implementations;
+using lab2.Domain.Models;
 
 namespace lab2.DataLayer
 {
@@ -11,8 +12,10 @@ namespace lab2.DataLayer
         static void Main(string[] args)
         {
             var config = new MapperConfiguration(cfg => {
-                cfg.CreateMap<lab2.Domain.User, User>();
-                cfg.CreateMap<User, lab2.Domain.User>();
+                cfg.CreateMap<lab2.Domain.Models.UserUpdateModel, lab2.DataLayer.Entities.User>();
+                cfg.CreateMap<lab2.Domain.Models.UserUpdateModel, lab2.Domain.User>();
+                cfg.CreateMap<lab2.DataLayer.Entities.User, lab2.Domain.User>();
+                cfg.CreateMap<lab2.Domain.User, lab2.DataLayer.Entities.User>();
             });
             Mapper mapper = new Mapper(config);
 
@@ -20,18 +23,18 @@ namespace lab2.DataLayer
 
             UserDataAccess userDataAccess = new UserDataAccess(context, mapper);
 
-            Console.Write(userDataAccess.GetById(1));
-            /*
+            lab2.Domain.User user = userDataAccess.Insert(new UserUpdateModel{ Email="123@456.ru", PasswordHash="qwe" });
 
-                User user = new User { Email = "qwerty@123.ru", PasswordHash="123" };
-                db.Add(user);
-                db.SaveChanges();
-                Console.WriteLine(user.Id);
+            Console.WriteLine("Created User:");
+            Console.WriteLine(user);
 
-                Note note = new Note { User=user, Title="qwe", Content="Rty" };
-                db.Add(note);
-                db.SaveChanges();
-            */
+            Console.WriteLine("\nGet from Database User:");
+            Console.WriteLine(userDataAccess.Get(new UserIdentityModel(1)));
+
+            lab2.Domain.User updatedUser = userDataAccess.Update(new UserIdentityModel(1), new UserUpdateModel{ Email="111@789.ru", PasswordHash="rty" });
+
+            Console.WriteLine("\nUpdated User:");
+            Console.Write(updatedUser);
         }
     }
 }
