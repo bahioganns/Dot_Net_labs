143abab3787816623c29f65e561a2fccb75e1ed5
Alex Kosh <sasha23969@gmail.com>
2021-04-01 02:17:14 +0300

    feat: Added BLL.Implementations

---
+++ BLL/Implementations/NoteService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Text.RegularExpressions;
+using DataAccess.Contracts;
+using Domain;
+using Domain.Models;
+using BLL.Contracts;
+
+namespace BLL.Implementations
+{
+    public class NoteService : INoteCreateService, INoteGetService, INoteUpdateService, INoteDeleteService
+    {
+        private INoteDataAccess NoteDataAccess { get; }
+
+        public NoteService(INoteDataAccess noteDataAccess)
+        {
+            this.NoteDataAccess = noteDataAccess;
+        }
+
+        public Note CreateNote(NoteUpdateModel note)
+        {
+            return this.NoteDataAccess.Insert(note);
+        }
+
+        public Note GetNote(NoteIdentityModel noteId)
+        {
+            return this.NoteDataAccess.Get(noteId);
+        }
+
+        public Note UpdateNote(NoteIdentityModel noteId, NoteUpdateModel note)
+        {
+            return this.NoteDataAccess.Update(noteId, note);
+        }
+
+        public void DeleteNote(NoteIdentityModel noteId)
+        {
+            this.NoteDataAccess.Delete(noteId);
+        }
+    }
+}
+
---
+++ BLL/Implementations/UserService.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Text.RegularExpressions;
+using DataAccess.Contracts;
+using Domain;
+using Domain.Models;
+using BLL.Contracts;
+
+namespace BLL.Implementations
+{
+    public class UserService : IUserCreateService, IUserGetService, IUserUpdateService, IUserDeleteService, IUserValidateService
+    {
+        private IUserDataAccess UserDataAccess { get; }
+
+        public UserService(IUserDataAccess userDataAccess)
+        {
+            this.UserDataAccess = userDataAccess;
+        }
+
+        public User CreateUser(UserUpdateModel user)
+        {
+            return this.UserDataAccess.Insert(user);
+        }
+
+        public User GetUser(UserIdentityModel userId)
+        {
+            return this.UserDataAccess.Get(userId);
+        }
+
+        public User UpdateUser(UserIdentityModel userId, UserUpdateModel user)
+        {
+            return this.UserDataAccess.Update(userId, user);
+        }
+
+        public void DeleteUser(UserIdentityModel userId)
+        {
+            this.UserDataAccess.Delete(userId);
+        }
+
+        public void validateUser(UserUpdateModel user)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if(!regex.Match(user.Email).Success)
+            {
+                throw new ArgumentException("Wrong email format");
+            }
+        }
+    }
+}
+
