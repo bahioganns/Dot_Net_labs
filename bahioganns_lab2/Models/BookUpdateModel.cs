28c6577b8a7237f51da45e2ac877f0091277eb38
Iogann Bah <bahioganns@gmail.com>
2021-03-31 19:17:59 +0300

    first update

---
+++ bahioganns_lab2/Models/BookUpdateModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bahioganns_lab2.Models
+{
+    class BookUpdateModel
+    {
+    }
+}
---
+++ bahioganns_lab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bahioganns_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
