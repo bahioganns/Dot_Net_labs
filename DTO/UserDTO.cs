e3e927c95ec72b034e1ced545cd995b30888f4db
Alex Kosh <sasha23969@gmail.com>
2021-04-03 02:55:57 +0300

    fix: Make Id nullable

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UserDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
     }
