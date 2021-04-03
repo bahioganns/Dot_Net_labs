4f6ee515f8926548e9f2554ed264d6aa6931ef54
Alex Kosh <sasha23969@gmail.com>
2021-04-03 20:23:32 +0300

    test: Added test for GetAllUsers

--- test_webapi.sh
+++ test_webapi.sh
@@ -1,11 +1,14 @@
 # Create User
-curl "http://localhost:5000/api/user" -X POST -H "Content-Type: application/json" --data '{"Email":"qwe@rty.ru","PasswordHash":"qwerty"}'
+curl "http://localhost:5000/api/user" -X POST -H "Content-Type: application/json" --data '{"Email":"q1we@rty.ru","PasswordHash":"qwerty"}'
 
 # Create Bad User
 curl "http://localhost:5000/api/user" -X POST -H "Content-Type: application/json" --data '{"Email":"@r.u","PasswordHash":"qwerty"}'
 
 # Get User by id
 curl "http://localhost:5000/api/user/2"
+
+# Get all Users
+curl "http://localhost:5000/api/user"
 
 # Update User Data
 curl "http://localhost:5000/api/user/1" -X PUT -H "Content-Type: application/json" --data '{"Email":"asd@aww.ru","PasswordHash":"123456"}'
